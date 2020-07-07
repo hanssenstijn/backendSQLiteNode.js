f46bf7f1b35185053ec4f0d4cca272565f19adf6
hanssenstijn <hanssenstijn@gmail.com>
2020-07-07 13:21:50 +0200

    retrieve both dataset data

--- src/controllers/provinceCon.js
+++ src/controllers/provinceCon.js
@@ -1,11 +1,17 @@
 const dal = require('../libs/db');
 const logger = require('../libs/logger');
 
+const getInhabitantsData = () => {
+  logger.info('Retrieving inhabitants data');
+  return dal.getDataFromTable('inhabitants');
+};
+
 const getProvinceData = () => {
-  logger.info('Retrieving location data');
+  logger.info('Retrieving infoProvince data');
   return dal.getDataFromTable('infoProvince');
 };
 
 module.exports = {
   getProvinceData,
+  getInhabitantsData
 }
--- src/routes/provinceRout.js
+++ src/routes/provinceRout.js
@@ -1,17 +1,18 @@
 const express = require("express");
 
 const logger = require('../libs/logger')
-const { getProvinceData } = require('../controllers/provinceCon');
+const { getProvinceData, getInhabitantsData } = require('../controllers/provinceCon');
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
   try {
     const provinceData = await getProvinceData();
-    if (!provinceData) {
-      throw new Error('Could not retrieve any location data.');
+    const inhabitantsData = await getInhabitantsData();
+    if (!provinceData || !inhabitantsData) {
+      throw new Error('Could not retrieve any data.');
     }
-    res.json(provinceData);
+    res.json([provinceData, inhabitantsData]);
   } catch (err) {
     logger.error(err.message);
     res.sendStatus(500);
