642f4ea26afaf04daf591f3f10a8fa8336825e66
hanssenstijn <hanssenstijn@gmail.com>
2020-07-09 14:48:35 +0200

    check content api response on typ and property

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 const app = require("../src/app");
 const chai = require("chai");
 const chaiHttp = require("chai-http");
+should = chai.should();
 
 const { getRowData } = require('../src/controllers/specificRowsCon');
 
@@ -24,6 +25,20 @@ describe("Server!", () => {
       .get("/api/mockupData")
       .end((err, res) => {
         expect(res).to.have.status(200);
+        done();
+      });
+  });
+  it("Checks reponse data api rowData", done => {
+    chai
+      .request(app)
+      .get("/api/rowData")
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        res.body.should.be.a('object');
+        res.body.should.have.property('name');
+        res.body.should.have.property('inhabitants');
+        res.body.should.have.property('name').eql('Noord-Brabant');
+        res.body.should.have.property('inhabitants').eql('2563432');
         done();
       });
   });
